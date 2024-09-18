63527c11a72d801f830558e313ae4fe4079ca8da
Satria Yusuf Saputra <111202113940@mhs.dinus.ac.id>
2024-09-18 18:18:09 +0700

    wes tak hapus

--- Dashboard/scripts.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.querySelector(".nav-links");
-    navLinks.classList.toggle("active");
-  }
-  
-  function cardClick(featureName) {
-    alert(featureName + " clicked!");
-  }
-  
